0718c2a970a25786a4d5b1c6dd929b0363ccdbb5
Shahid Rashid Bhat <sr14201721@gmail.com>
2023-11-17 12:26:24 +0530

    add seperate delete functionality and update css

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ToDoItem(props) {
+
+  const [isLineThrough, setLine] = useState(false);
+
+  function lineThrough() {
+    setLine(!isLineThrough);
+  }
+
   return (
     <div
+      className="todoItem"
+    >
+      <li 
+      onClick={lineThrough}
+      style={{textDecoration: isLineThrough && "line-through"}}
+      >{props.text}</li>
+      
+      <p
       onClick={() => {
         props.onChecked(props.id);
       }}
-      className="todoItem"
-    >
-      <li>{props.text}</li>
+      >D</p>
     </div>
   );
 }
